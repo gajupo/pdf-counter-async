20353106ed153d7b3fb2f6b6d5fa45369508404d
Gaudencio Jurado <gjurado@virtualidevs.com>
2021-06-17 18:16:19 -0500

    PDF Counter for files and pages using C#

---
+++ PDFCounter/PDFCounter.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace PDFCounter
+{
+    partial class PDFCounter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblIndicator = new System.Windows.Forms.Label();
+            this.txtPDFFolder = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblResultPages = new System.Windows.Forms.Label();
+            this.lblResultFiles = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pdfFolderdialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblErrors = new System.Windows.Forms.Label();
+            this.GesFilesFromSystem = new System.ComponentModel.BackgroundWorker();
+            this.CountPDFPages = new System.ComponentModel.BackgroundWorker();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 46.08696F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 53.91304F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 65F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(976, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblIndicator);
+            this.panel1.Controls.Add(this.txtPDFFolder);
+            this.panel1.Controls.Add(this.btnBrowse);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(970, 171);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblIndicator
+            // 
+            this.lblIndicator.AutoSize = true;
+            this.lblIndicator.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIndicator.Location = new System.Drawing.Point(262, 90);
+            this.lblIndicator.Name = "lblIndicator";
+            this.lblIndicator.Size = new System.Drawing.Size(100, 25);
+            this.lblIndicator.TabIndex = 3;
+            this.lblIndicator.Text = "Working...";
+            this.lblIndicator.Visible = false;
+            // 
+            // txtPDFFolder
+            // 
+            this.txtPDFFolder.Location = new System.Drawing.Point(262, 59);
+            this.txtPDFFolder.Name = "txtPDFFolder";
+            this.txtPDFFolder.Size = new System.Drawing.Size(514, 22);
+            this.txtPDFFolder.TabIndex = 2;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(810, 55);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(113, 30);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(83, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Folder Location";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.lblErrors);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.lblResultPages);
+            this.panel2.Controls.Add(this.lblResultFiles);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 180);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(970, 201);
+            this.panel2.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(265, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Errors";
+            // 
+            // lblResultPages
+            // 
+            this.lblResultPages.AutoSize = true;
+            this.lblResultPages.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultPages.Location = new System.Drawing.Point(407, 76);
+            this.lblResultPages.Name = "lblResultPages";
+            this.lblResultPages.Size = new System.Drawing.Size(23, 25);
+            this.lblResultPages.TabIndex = 3;
+            this.lblResultPages.Text = "0";
+            // 
+            // lblResultFiles
+            // 
+            this.lblResultFiles.AutoSize = true;
+            this.lblResultFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultFiles.Location = new System.Drawing.Point(407, 40);
+            this.lblResultFiles.Name = "lblResultFiles";
+            this.lblResultFiles.Size = new System.Drawing.Size(23, 25);
+            this.lblResultFiles.TabIndex = 2;
+            this.lblResultFiles.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(265, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Pages Count";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(265, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Files Count";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnStart);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(3, 387);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(970, 60);
+            this.panel3.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(864, 0);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(106, 60);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblErrors
+            // 
+            this.lblErrors.AutoSize = true;
+            this.lblErrors.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblErrors.Location = new System.Drawing.Point(407, 108);
+            this.lblErrors.Name = "lblErrors";
+            this.lblErrors.Size = new System.Drawing.Size(23, 25);
+            this.lblErrors.TabIndex = 5;
+            this.lblErrors.Text = "0";
+            // 
+            // GesFilesFromSystem
+            // 
+            this.GesFilesFromSystem.DoWork += new System.ComponentModel.DoWorkEventHandler(this.GesFilesFromSystem_DoWork);
+            this.GesFilesFromSystem.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.GesFilesFromSystem_RunWorkerCompleted);
+            // 
+            // CountPDFPages
+            // 
+            this.CountPDFPages.DoWork += new System.ComponentModel.DoWorkEventHandler(this.CountPDFPages_DoWork);
+            this.CountPDFPages.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.CountPDFPages_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(976, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "PDF Counter";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.FolderBrowserDialog pdfFolderdialog;
+        private System.Windows.Forms.Label lblIndicator;
+        private System.Windows.Forms.TextBox txtPDFFolder;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblResultPages;
+        private System.Windows.Forms.Label lblResultFiles;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblErrors;
+        private System.ComponentModel.BackgroundWorker GesFilesFromSystem;
+        private System.ComponentModel.BackgroundWorker CountPDFPages;
+    }
+}
+
---
+++ PDFCounter/PDFCounter.cs
@@ -0,0 +1,173 @@
+﻿using iTextSharp.text.pdf;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDFCounter
+{
+    public partial class PDFCounter : Form
+    {
+        public string SelectedPath { get; set; }
+        public List<string> FilesFound { get; set; } = new List<string>();
+        public int ErrorsCount { get; set; }
+        public delegate void ShowInProgressTask();
+        public delegate void ShowFilesFound();
+        public delegate void ShowCurrentStatusPages(string count);
+        public delegate void ShowCurrentStatusFiles(string count);
+        delegate void MsgShowErrors(string message);
+        public PDFCounter()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            pdfFolderdialog.RootFolder = Environment.SpecialFolder.MyComputer;
+            pdfFolderdialog.ShowDialog();
+            SelectedPath = pdfFolderdialog.SelectedPath;
+            txtPDFFolder.Text = SelectedPath;
+            btnStart.Enabled = false;
+            GesFilesFromSystem.RunWorkerAsync();
+
+        }
+
+        private void GesFilesFromSystem_DoWork(object sender, DoWorkEventArgs e)
+        {
+            object[] argsObjectError = new object[1];
+            BeginInvoke(new ShowInProgressTask(this.ShowInProgress));
+            try
+            {
+                FilesFound = Directory.GetFiles(SelectedPath, "*.pdf*", SearchOption.AllDirectories).ToList();
+            }
+            catch (Exception ex)
+            {
+                argsObjectError[0] = ex.Message;
+                BeginInvoke(new MsgShowErrors(this.ShowMsgError), argsObjectError);
+                
+            }
+        }
+
+        private void GesFilesFromSystem_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            BeginInvoke(new ShowFilesFound(this.ShowCompletedSearch));
+
+        }
+        void ShowInProgress()
+        {
+            lblIndicator.Visible = true;
+            lblIndicator.Text = "Working...";
+        }
+        void ShowCompletedSearch()
+        {
+            lblIndicator.ForeColor = Color.DarkBlue;
+            lblIndicator.Text = $"Files found: {FilesFound.Count}";
+            btnStart.Enabled = true;
+            
+        }
+        void ShowCountingPagesIndicator()
+        {
+            lblIndicator.Text = "Counting Pages and Files";
+        }
+        void ShowCurrentPagesStr(string count)
+        {
+            lblResultPages.Text = count;
+
+        }
+        void ShowCurrentFilesStr(string count)
+        {
+            lblResultFiles.Text = count;
+
+        }
+        void ShowCurrentErrors(string count)
+        {
+            lblErrors.Text = count;
+        }
+        void ShowCompletedProcess()
+        {
+            lblIndicator.Text = "Process Completed";
+            btnStart.Enabled = true;
+            btnBrowse.Enabled = true;
+        }
+        void ShowMsgError(string message)
+        {
+            MessageBox.Show(message, "Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        private void CountPDFPages_DoWork(object sender, DoWorkEventArgs e)
+        {
+            object[] argsObjectErrors = new object[1];
+            try
+            {
+                object[] argsObjectPages = new object[1];
+                object[] argsObjectFiles = new object[1];
+                int fileCounter = 0;
+                int pageCounter = 0;
+                foreach (var filePath in FilesFound)
+                {
+                    try
+                    {
+                        using (PdfReader pdfReader = new PdfReader(filePath))
+                        {
+                            pageCounter += pdfReader.NumberOfPages;
+                        }
+                        argsObjectPages[0] = pageCounter.ToString();
+                        BeginInvoke(new ShowCurrentStatusPages(this.ShowCurrentPagesStr), argsObjectPages);
+
+                        fileCounter++;
+                        argsObjectFiles[0] = fileCounter.ToString();
+                        BeginInvoke(new ShowCurrentStatusFiles(this.ShowCurrentFilesStr), argsObjectFiles);
+                    }
+                    catch (iTextSharp.text.exceptions.BadPasswordException)
+                    {
+                        ErrorsCount++;    
+                    }
+                    catch(iTextSharp.text.exceptions.InvalidPdfException)
+                    {
+                        ErrorsCount++;
+                    }
+                    argsObjectErrors[0] = ErrorsCount.ToString();
+                    BeginInvoke(new ShowCurrentStatusFiles(this.ShowCurrentErrors), argsObjectErrors);
+
+                }
+            }
+            catch (AggregateException aex)
+            {
+                throw aex;
+            }
+            
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (FilesFound.Count > 0)
+            {
+                try
+                {
+                    CountPDFPages.RunWorkerAsync();
+                    ShowCountingPagesIndicator();
+                    btnStart.Enabled = false;
+                    btnBrowse.Enabled = false;
+                }
+                catch (Exception ex)
+                {
+
+                    ShowMsgError(ex.Message);
+                }
+                
+            }
+            
+        }
+
+        private void CountPDFPages_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            BeginInvoke(new ShowFilesFound(this.ShowCompletedProcess));
+        }
+    }
+}
